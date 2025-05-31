a8b0aa99af68cea02b638f9a5f478f13ae376fd5
pedrosnaves <pedrosnaves@gmail.com>
2025-05-31 12:02:41 -0300

    Create Dama.java

---
+++ xadrez_flyweight/Dama.java
@@ -0,0 +1,40 @@
+// Subclasse de PecaXadrez que instacia cada objeto dama criado
+
+public class Dama extends PecaXadrez {
+    public Dama(String cor) {
+        super(cor);
+    }
+
+    
+    public String getTipo() {
+        return "Dama";
+    }
+
+   
+    public boolean movimentoValido(int[] origem, int[] destino, PecaXadrez[][] tabuleiro) {
+        int deltaLinha = destino[0] - origem[0];
+        int deltaColuna = destino[1] - origem[1];
+
+        
+        if (deltaLinha != 0 && deltaColuna != 0 && Math.abs(deltaLinha) != Math.abs(deltaColuna)) {
+            return false; 
+        }
+
+        int passoLinha = Integer.compare(deltaLinha, 0);
+        int passoColuna = Integer.compare(deltaColuna, 0);
+
+        int linha = origem[0] + passoLinha;
+        int coluna = origem[1] + passoColuna;
+        while (linha != destino[0] || coluna != destino[1]) {
+            if (tabuleiro[linha][coluna] != null) {
+                return false; 
+            }
+            linha += passoLinha;
+            coluna += passoColuna;
+        }
+
+        
+        return (tabuleiro[destino[0]][destino[1]] == null ||
+                !tabuleiro[destino[0]][destino[1]].getCor().equals(this.cor));
+    }
+}
