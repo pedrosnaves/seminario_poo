f005ad38e0ee69886002004589f99ae7baf4c38a
pedrosnaves <pedrosnaves@gmail.com>
2025-05-31 12:04:11 -0300

    Create PosicaoPeca.java

---
+++ xadrez_flyweight/PosicaoPeca.java
@@ -0,0 +1,14 @@
+public class PosicaoPeca {
+    private PecaXadrez peca; // Objeto peça, que possui atributos iguais para peças do mesmo tipo
+    private String posicao; // Atributo que irá diferenciar os objetos do mesmo tipo (Flyweight)
+
+    public PosicaoPeca(String posicao, PecaXadrez peca) {
+        this.posicao = posicao;
+        this.peca = peca;
+    }
+
+    public void exibir() {
+        System.out.println(peca.getTipo() + " (" + peca.getCor() + ") em " + posicao);
+    }
+    
+}
