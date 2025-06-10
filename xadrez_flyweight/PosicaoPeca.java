5ade31a78e91ac0f0ae3536595298051f450d410
pedrosnaves <pedrosnaves@gmail.com>
2025-06-10 10:19:51 -0300

    Update PosicaoPeca.java

--- xadrez_flyweight/PosicaoPeca.java
+++ xadrez_flyweight/PosicaoPeca.java
@@ -1,8 +1,8 @@
 //Classe para atribuir o atributo posicao a pecas do mesmo tipo e implementar o uso do Flyweight
 
 public class PosicaoPeca {
-    private PecaXadrez peca; // Objeto peça, que possui atributos iguais para peças do mesmo tipo
-    private String posicao; // Atributo que irá diferenciar os objetos do mesmo tipo (Flyweight)
+    private PecaXadrez peca; // Atributo intrínseco -> Objeto peça, que possui atributos iguais para peças do mesmo tipo
+    private String posicao; // Atributo extrínseco -> Atributo que irá diferenciar os objetos do mesmo tipo (Flyweight)
 
     public PosicaoPeca(String posicao, PecaXadrez peca) {
         this.posicao = posicao;
